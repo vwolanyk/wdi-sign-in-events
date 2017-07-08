0f74a7380f496e7fc1a93436f27a8b02059d9340
Vlad Wolanyk <vladwolanyk@yahoo.ca>
2017-07-08 11:48:59 -0400

    Fade out on close button works

--- js/main.js
+++ js/main.js
@@ -2,11 +2,17 @@ document.addEventListener('DOMContentLoaded', function(){
 
 var signIn = document.querySelector("button.signin");
 var modal = document.querySelector('div.modal');
-
+var closeButton = document.querySelector('button.close')
 signIn.addEventListener('click',function(){ console.log("sign In Clicked")
 
 modal.className = "fadeIn";
 });
 
+closeButton.addEventListener('click',function(){
+  console.log("close clicked");
+  modal.classList.remove("fadeIn")
+  modal.classList.add("fadeOut")
+  setTimeout(function(){modal.className = "modal";},1000);
+});
 
 });
